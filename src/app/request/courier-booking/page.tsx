51b3985d98ea328218cf930c8de237d5b8ec0227
Mikhail Volodin <volodinmiisha1@gmail.com>
2025-09-05 15:22:37 +0300

    Fixed: Определение локации 26

--- src/app/request/courier-booking/page.tsx
+++ src/app/request/courier-booking/page.tsx
@@ -550,7 +550,7 @@ const CourierBookingPage = () => {
                                             side="top" 
                                             sideOffset={8}
                                             alignOffset={0}
-                                            className="w-[320px] sm:w-[450px] max-w-[95vw] p-2 sm:p-4"
+                                            className="w-[380px] sm:w-[500px] max-w-[95vw] p-3 sm:p-4"
                                         >
                                             <Calendar
                                                 mode="single"
