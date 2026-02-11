b51fc028f6f988c0a3651aa8593b1cadfc36df2e
Mikhail Volodin <volodinmiisha1@gmail.com>
2026-02-11 12:06:59 +0300

    Deleted page help. Added FAQ on main page. Remove auth for users. Added page for admin auth.2

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline-first service worker for Next.js app shell */
-const CACHE_NAME = 'qoqos-cache-v1-4-251'
+const CACHE_NAME = 'qoqos-cache-v1-4-252'
 const APP_SHELL = [
   '/',
   '/manifest.webmanifest',
