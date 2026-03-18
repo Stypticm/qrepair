329ea8aff39326d8b1d9b9cc6a850343ca2d8543
Mikhail Volodin <volodinmiisha1@gmail.com>
2026-03-18 11:27:18 +0300

    Added some changes to opennext config and wrangler.json 3

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /* Minimal offline-first service worker for Next.js app shell */
-const CACHE_NAME = 'qoqos-cache-v1-4-370'
+const CACHE_NAME = 'qoqos-cache-v1-4-372'
 const APP_SHELL = [
   '/',
   '/manifest.webmanifest',
